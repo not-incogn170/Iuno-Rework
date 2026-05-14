6cb8d97fdce3db86b19912a0e8558ca3bdd570ef
incognitoDev <72125799+not-incogn170@users.noreply.github.com>
2026-05-14 12:23:16 +0700

    test

--- index.js
+++ index.js
@@ -98,9 +98,7 @@ async function main(){
       case '.s':
         if(!msg.message.imageMessage && !msg.message.videoMessage){
           await sock.sendMessage(jid, {text: 'No media found, please attach image/video'}, {quoted: msg})
-        }else if(msg.message.imageMessage){
-          sticker.fromImage(sock, jid, msg, downloadMediaMessage)
-        }else if(msg.message.videoMessage){
+        }else{
           sticker.fromVideo(sock, jid, msg, downloadMediaMessage)
         }
         break
