097e7ee6fc8bfff7498c9d1e8304d2ba655bb652
not-Incogn170 <mcg027yt@gmail.com>
2026-08-06 23:46:58 +0700

    stuff

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ async function main(){
       case 'Bub':
         if(userId == `${OWNER_PHONE_NUMBER}@s.whatsapp.net`){
           await sock.sendMessage(jid, {text: 'iya bub💕'}, {quoted: msg})
-        }else await sock.sendMessage(jid, {text: 'My bubub cuman ' + `@${OWNER_PHONE_NUMBER}` + ' seorang'}, {quoted: msg})
+        }else await sock.sendMessage(jid, {text: 'My bubub cuman ' + `@${OWNER_PHONE_NUMBER}` + ' seorang'}, {quoted: msg}, {mentions: [`${OWNER_PHONE_NUMBER}@s.whatsapp.net`]})
         break
       case 'kapan':
       case 'Kapan':
